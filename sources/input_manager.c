d9ba4051c0abb3e729d7fce210f9680699e0887d
Benjamin Pisano <benjamin.pisano@icloud.com>
2018-03-20 18:42:27 +0100

    fixes

--- sources/input_manager.c
+++ sources/input_manager.c
@@ -6,7 +6,7 @@
 /*   By: bpisano <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/03/07 20:05:58 by bpisano      #+#   ##    ##    #+#       */
-/*   Updated: 2018/03/20 12:33:59 by bpisano     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/03/20 18:40:24 by bpisano     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -19,7 +19,7 @@ int		skip_next_line(const int fd)
 	char	*line;
 
 	ret = get_next_line(fd, &line);
-	if (ret > 0)
+	if (ret != -1)
 		free(line);
 	return (ret);
 }
